b4939495130b056207ae32746bc4f1eb4732002c
sharonkass <kasssharon@gmail.com>
2017-02-08 08:22:35 -0500

    Add screens for viewing and adding match reports

---
+++ FRCScout2017/AddMatchViewController.swift
@@ -0,0 +1,114 @@
+//
+//  AddMatchViewController.swift
+//  FRCScout2017
+//
+//  Created by Sharon Kass on 2/5/17.
+//  Copyright © 2017 RoboTigers. All rights reserved.
+//
+
+import UIKit
+
+class AddMatchViewController: UIViewController {
+    
+    // MARK: - Data passed in from segue
+    
+    var selectedTournament : Int16 = 0
+    
+    // MARK: - Outlets and Actions for components of add-match scene
+    
+    @IBOutlet weak var match: UITextField!
+    @IBOutlet weak var team: UITextField!
+    @IBOutlet weak var autoCrosses: UISwitch!
+    @IBOutlet weak var autoScoresGear: UISwitch!
+    @IBOutlet weak var autoFuelHighLabel: UILabel!
+    @IBOutlet weak var gearCycle: UISegmentedControl!
+    // SABRINA: Plesae continue here wiring up all the outlets
+    
+    // SABRINA: Please continue here wiring up all the actions
+    @IBAction func autoHighFuelStepper(_ sender: UIStepper) {
+        autoFuelHighLabel.text = String((Int(sender.value).description))
+    }
+    
+    // MARK: - View functions
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("AddMatchViewController - view loaded with selectedTourname = \(selectedTournament)")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func displayErrorAlertWithOk(_ msg: String) {
+        let refreshAlert = UIAlertController(title: "Error", message: msg, preferredStyle: UIAlertControllerStyle.alert)
+        
+        refreshAlert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction!) in
+            print("Notify user of error")
+            return
+        }))
+        
+        DispatchQueue.main.async(execute: {
+            self.present(refreshAlert, animated: true, completion: nil)
+        })
+    }
+    
+    
+    // MARK: - Cancel action
+    
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        let refreshAlert = UIAlertController(title: "Are you sure?", message: "Data on this screen will be lost if you do not save first.", preferredStyle: UIAlertControllerStyle.alert)
+        
+        refreshAlert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction!) in
+            print("Cancel from add-match scene")
+            self.dismiss(animated: true, completion: nil)
+        }))
+        
+        refreshAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action: UIAlertAction!) in
+            print("Cancel the cancel, stay on the screen")
+        }))
+        
+        present(refreshAlert, animated: true, completion: nil)
+    }
+    
+
+    // MARK: - Save action
+    
+    /*
+     * Save the entered data into the data store. First check for required fields.
+     *
+     * The unique key for a match report record is:
+     *    Tournament_Match_Team
+     */
+    @IBAction func save(_ sender: UIBarButtonItem) {
+        print("Save match record to data store")
+        if ((match.text)!.isEmpty || (team.text)!.isEmpty) {
+            displayErrorAlertWithOk("Match and Team Number is required!")
+        } else {
+            // save the report to the data store
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+                return
+            }
+            let context = appDelegate.persistentContainer.viewContext
+            let matchReport = MatchReport(context: context)
+            let uniqueIdentifier = "\(selectedTournament)_\(match.text!)_\(team.text!)"
+            matchReport.tournament = selectedTournament
+            matchReport.matchNumber = match.text!
+            matchReport.teamNumber = team.text!
+            matchReport.uniqueIdentifier = uniqueIdentifier
+            matchReport.autoCrossedLine = autoCrosses.isOn
+            matchReport.autoFuelHigh = (Int16(autoFuelHighLabel.text!))!
+            matchReport.gearCycle = Int16(gearCycle.selectedSegmentIndex)
+            // SABRINA: Keep going from here please add the entity attributes to matchReport so it can be stored to data store
+            do {
+                print("Save match record: \(matchReport)")
+                try context.save()
+            } catch let error as NSError {
+                print("Could not save the match report. \(error), \(error.userInfo)")
+            }
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+
+}
---
+++ FRCScout2017/MatchesViewController.swift
@@ -0,0 +1,152 @@
+//
+//  MatchesViewController.swift
+//  FRCScout2017
+//
+//  Created by Sharon Kass on 2/5/17.
+//  Copyright © 2017 RoboTigers. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class MatchesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tournamentSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var matchesTableView: UITableView!
+    
+    // MARK: - Data model for this scene
+    
+    var matches: [MatchReport] = []
+    
+    // MARK: - View containing the segmented control and the table view
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        refreshMatches()
+        matchesTableView.reloadData()
+    }
+    
+    
+    // MARK: - Unwind Actions from Add Report scene
+    
+    @IBAction func unwindFromMatchReportCancel(_ unwindSegue: UIStoryboardSegue) {
+        print("Canceled")
+    }
+
+    
+    // MARK: - Navigation
+    
+    @IBAction func addMatchReport(_ sender: UIBarButtonItem) {
+        print("Add new match report")
+        performSegue(withIdentifier: "AddMatchReportSegue", sender: self)
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        var destination = segue.destination
+        if let navigationController = destination as? UINavigationController {
+            destination = navigationController.visibleViewController!
+        }
+        
+        if let segueIdentifier = segue.identifier {
+            switch segueIdentifier {
+            case "AddMatchReportSegue":
+                // Send the selected tournament number into the AddMatch scene so the 
+                // report can be saved to the proper tournament
+                if let addMatchViewController = destination as? AddMatchViewController {
+                    print ("AddMatchReportSegue segue")
+                    addMatchViewController.selectedTournament = Int16(tournamentSegmentedControl.selectedSegmentIndex)
+                }
+            default:
+                print ("Unknown segueIdentifier: \(segueIdentifier)")
+                
+            }
+        }
+    }
+ 
+    // MARK: - Table View
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return matches.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MatchReportCell", for: indexPath)
+        let match = matches[indexPath.row]
+        cell.textLabel?.text = match.uniqueIdentifier
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+                return
+            }
+            let context = appDelegate.persistentContainer.viewContext
+            let match = matches[indexPath.row]
+            context.delete(match)
+            do {
+                try context.save()
+            } catch let error as NSError {
+                print("Could not delete match row. \(error), \(error.userInfo)")
+            }
+            
+            // Update our global variable
+            matches.remove(at: indexPath.row)
+            
+            // Delete the row from the table view
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    // MARK: - Data selection via segmented control
+
+    /*
+     * Fill the matches array with data from the data store for the selected tournament
+     */
+    @IBAction func chooseTournament(_ sender: UISegmentedControl) {
+    
+        switch(tournamentSegmentedControl.selectedSegmentIndex) {
+        case 0:
+            print("Fetching matches for tournament to 0 (Regional)")
+            break
+        case 1:
+            print("Fetching matches for tournament to 1 (LI)")
+            break
+        case 2:
+            print("Fetching matches for tournament to 2 (Championship)")
+            break
+        default:
+            print("No tournament setting, should never reach this!")
+            break
+        }
+        
+        refreshMatches()
+
+        matchesTableView.reloadData()
+    }
+    
+    private func refreshMatches() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<MatchReport>(entityName: "MatchReport")
+        fetchRequest.predicate = NSPredicate(format: "tournament == \(tournamentSegmentedControl.selectedSegmentIndex)")
+        do {
+            matches = try context.fetch(fetchRequest)
+            print("Retrieved \(matches.count) matches")
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+}
