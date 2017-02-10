506ed0cf3c65ac0aeb341d5b8a3b2985ff9001e1
sharonkass <kasssharon@gmail.com>
2017-02-10 07:34:15 -0500

    Add navigation for Teams which leads to Pit Report

---
+++ FRCScout2017/PitViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PitViewController.swift
+//  FRCScout2017
+//
+//  Created by Sharon Kass on 2/10/17.
+//  Copyright © 2017 RoboTigers. All rights reserved.
+//
+
+import UIKit
+
+class PitViewController: UIViewController {
+    
+    var selectedTeam = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "\(selectedTeam) Pit Report"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
