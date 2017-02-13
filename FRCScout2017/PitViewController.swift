05e0252cee2b5742613459b323b4693879846664
sharonkass <kasssharon@gmail.com>
2017-02-13 05:36:45 -0500

    Enhance pit scene 1) fix save button to dismiss scene after saving 2) allow updating of existing pit report 3) ensure a team was picked before proceeding with the pit report

--- FRCScout2017/PitViewController.swift
+++ FRCScout2017/PitViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import CoreData
 
 class PitViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
-    var selectedTeam = ""
-    var selectedDriveTrainType = "Type 1"
+    var selectedTeamNumber = ""
+    var selectedDriveTrainType = ""
+    var existingPitReport : PitReport?
     
     let driveTrainTypes = ["Tank", "H Drive", "Omni", "Halo", "Arcade"]
 
@@ -24,26 +26,29 @@ class PitViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDat
     @IBOutlet weak var crossesLineSwitch: UISwitch!
     
     @IBAction func save(_ sender: UIBarButtonItem) {
-        print("Save pit record to data store")
-        // save the report to the data store
+        // save the report to the data store either using a new object to updating an existing object
+        var pitRecord : PitReport? = nil
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
             return
         }
         let context = appDelegate.persistentContainer.viewContext
-        let pitRecord = PitReport(context: context)
-        pitRecord.teamNumber = selectedTeam
-        pitRecord.contactName = contactName.text!
-        pitRecord.driveTrainType = selectedDriveTrainType
-        pitRecord.driveTrainMotorType = Int16(driveTrainMotorType.selectedSegmentIndex)
-        pitRecord.autoCross = crossesLineSwitch.isOn
+        if (existingPitReport != nil) {
+            pitRecord = existingPitReport
+        } else {
+            pitRecord = PitReport(context: context)
+        }
+        pitRecord?.teamNumber = selectedTeamNumber
+        pitRecord?.contactName = contactName.text!
+        pitRecord?.driveTrainType = selectedDriveTrainType
+        pitRecord?.driveTrainMotorType = Int16(driveTrainMotorType.selectedSegmentIndex)
+        pitRecord?.autoCross = crossesLineSwitch.isOn
         print("Pit Record is: \(pitRecord)")
-        
-            do {
-                print("Save pit record: \(pitRecord))")
-                try context.save()
-            } catch let error as NSError {
-                print("Could not save the pit report. \(error), \(error.userInfo)")
-            }
+        do {
+            print("Save pit record: \(pitRecord))")
+            try context.save()
+        } catch let error as NSError {
+            print("Could not save the pit report. \(error), \(error.userInfo)")
+        }
         self.dismiss(animated: true, completion: nil)
     }
     
@@ -62,11 +67,46 @@ class PitViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDat
         present(refreshAlert, animated: true, completion: nil)
     }
     
+    // MARK: - View Controller
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Team \(selectedTeam) Pit Report"
         driveTrainTypePicker.dataSource = self
         driveTrainTypePicker.delegate = self
+        
+        // Set title to indicate the selected team entered via the segue
+        title = "Team \(selectedTeamNumber) Pit Report"
+        
+        if (selectedTeamNumber == "") {
+            displayErrorAlertWithOk("Please pick a team first")
+            self.dismiss(animated: true, completion: nil)
+        } else {
+            // Get pit report for the selected team, if a report exists in the data store
+            // There should only be one pit report but use an array here to be safe
+            var pitReports: [PitReport] = []
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+                return
+            }
+            let context = appDelegate.persistentContainer.viewContext
+            let fetchRequest = NSFetchRequest<PitReport>(entityName: "PitReport")
+            fetchRequest.predicate = NSPredicate(format: "teamNumber == \(selectedTeamNumber)")
+            do {
+                pitReports = try context.fetch(fetchRequest)
+                print("SHARON: Found \(pitReports.count) pitReports in data store")
+                if pitReports.count > 0 {
+                    // There should only be one pit report for a team but if more are found
+                    // then just ignore them - we take the first (0th position) array element
+                    existingPitReport = pitReports[0]
+                    print("Existig pit report found: \(existingPitReport)")
+                    print("And its contact is \(existingPitReport?.contactName)")
+                    contactName.text = existingPitReport?.contactName
+                }
+            } catch let error as NSError {
+                print("Could not fetch. \(error), \(error.userInfo)")
+            }
+        }
+        
+        
     }
     
     // MARK: - Picker
@@ -99,5 +139,21 @@ class PitViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDat
         // Pass the selected object to the new view controller.
     }
     */
+    
+    // MARK: - Utilities
+    
+    func displayErrorAlertWithOk(_ msg: String) {
+        let refreshAlert = UIAlertController(title: "Error", message: msg, preferredStyle: UIAlertControllerStyle.alert)
+        
+        refreshAlert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction!) in
+            print("Notify user of error")
+            return
+        }))
+        
+        DispatchQueue.main.async(execute: {
+            self.present(refreshAlert, animated: true, completion: nil)
+        })
+    }
+    
 
 }
